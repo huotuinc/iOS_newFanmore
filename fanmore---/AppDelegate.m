d5b775b7e57709d41bd6d7153c482e53807621e9
sxluohaibo <lhb@lhbdeMac-mini.local>
2015-06-26 15:34:16 +0800

    dasdasdadsasdasdasdasd

--- fanmore---/AppDelegate.m
+++ fanmore---/AppDelegate.m
@@ -207,6 +207,12 @@
                 NSString *fileName = [path stringByAppendingPathComponent:InitGlobalDate];
                 [NSKeyedArchiver archiveRootObject:resultData.global toFile:fileName]; //保存用户信息
                 
+              
+                
+                //1、保存个人信息
+                fileName = [path stringByAppendingPathComponent:LocalUserDate];
+                [NSKeyedArchiver archiveRootObject:nil toFile:fileName]; //保存用户信息
+                
             }else{
                 NSString * flag = @"right";
                 [[NSUserDefaults standardUserDefaults] setObject:flag forKey:loginFlag];
