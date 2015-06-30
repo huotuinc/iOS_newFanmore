4e8193077f12e9d505819162073fabcf60e592b6
HuoTu-Mac <aladdin@aladdindeimac.local>
2015-06-30 15:06:01 +0800

    321312312312

--- fanmore---/classes/TodayForeshow(今天预告)/controller/TodayForesController.m
+++ fanmore---/classes/TodayForeshow(今天预告)/controller/TodayForesController.m
@@ -57,6 +57,9 @@ static NSString *homeCellidentify = @"ForeshowTableViewCell.h";
     [self.tableView removeSpaces];
     //集成刷新控件
     [self setupRefresh];
+    
+    //注册转跳通知
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(operWebViewCn:) name:ReciveTaskId object:nil];
 }
 
 
@@ -239,6 +242,37 @@ static NSString *homeCellidentify = @"ForeshowTableViewCell.h";
             
         }
     }
+}
+
+
+//通知专跳
+- (void)operWebViewCn:(NSNotification *) notification {
+    NSLog(@"%@",notification);
+    UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+    
+    detailViewController *detailVc = [storyboard instantiateViewControllerWithIdentifier:@"detailViewController"];
+    detailVc.taskId = (int)notification.userInfo[@"id"]; //获取问题编号
+    detailVc.type = (int)notification.userInfo[@"type"];  //答题类型
+    detailVc.detailUrl = notification.userInfo[@"detailUrl"];//网页详情的url
+    detailVc.backTime = (int)notification.userInfo[@"backTime"];
+    detailVc.flay = [notification.userInfo[@"flay"] floatValue];
+    detailVc.shareUrl = notification.userInfo[@"shareUrl"];
+    detailVc.titless = notification.userInfo[@"title"];
+    detailVc.pictureUrl = notification.userInfo[@"pictureUrl"];
+    if ((int)notification.userInfo[@"type"] == 1) {
+        detailVc.title = @"答题任务";
+    }else if((int)notification.userInfo[@"type"] == 2){
+        detailVc.title = @"报名任务";
+    }else if((int)notification.userInfo[@"type"] == 3){
+        detailVc.title = @"画册类任务";
+    }else{
+        detailVc.title = @"游戏类任务";
+    }
+    ([notification.userInfo[@"reward"] floatValue] > 0|(int)notification.userInfo[@"taskFailed"]>0)?(detailVc.ishaveget=YES):(detailVc.ishaveget=NO);
+    
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:ReciveTaskId object:nil];
+    [self.navigationController pushViewController:detailVc animated:YES];
+    
 }
 
 @end
--- fanmore---/classes/home(主页)/controller/HomeViewController.m
+++ fanmore---/classes/home(主页)/controller/HomeViewController.m
@@ -116,6 +116,7 @@ static NSString * homeCellidentify = @"homeCellId";
     
     [self.tableView registerNib:[UINib nibWithNibName:@"HomeCell" bundle:nil] forCellReuseIdentifier:homeCellidentify];
     
+    //注册转跳通知
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(operWebViewCn:) name:ReciveTaskId object:nil];
 }
 
@@ -464,8 +465,34 @@ static NSString * homeCellidentify = @"homeCellId";
     
 }
 
+- (void)operWebViewCn:(NSNotification *) notification {
+    NSLog(@"%@",notification);
+    UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+    
+    detailViewController *detailVc = [storyboard instantiateViewControllerWithIdentifier:@"detailViewController"];
+    detailVc.taskId = (int)notification.userInfo[@"id"]; //获取问题编号
+    detailVc.type = (int)notification.userInfo[@"type"];  //答题类型
+    detailVc.detailUrl = notification.userInfo[@"detailUrl"];//网页详情的url
+    detailVc.backTime = (int)notification.userInfo[@"backTime"];
+    detailVc.flay = [notification.userInfo[@"flay"] floatValue];
+    detailVc.shareUrl = notification.userInfo[@"shareUrl"];
+    detailVc.titless = notification.userInfo[@"title"];
+    detailVc.pictureUrl = notification.userInfo[@"pictureUrl"];
+    if ((int)notification.userInfo[@"type"] == 1) {
+        detailVc.title = @"答题任务";
+    }else if((int)notification.userInfo[@"type"] == 2){
+        detailVc.title = @"报名任务";
+    }else if((int)notification.userInfo[@"type"] == 3){
+        detailVc.title = @"画册类任务";
+    }else{
+        detailVc.title = @"游戏类任务";
+    }
+    ([notification.userInfo[@"reward"] floatValue] > 0|(int)notification.userInfo[@"taskFailed"]>0)?(detailVc.ishaveget=YES):(detailVc.ishaveget=NO);
+    
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:ReciveTaskId object:nil];
+    [self.navigationController pushViewController:detailVc animated:YES];
 
-
+}
 
 
 - (void)answerOverToreferch{
