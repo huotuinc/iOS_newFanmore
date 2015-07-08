3fb55f932e9b1e1d97e6630e92beebdd6171daee
HuoTu-Mac <aladdin@aladdindeimac.local>
2015-07-08 21:02:27 +0800

    312312312

--- fanmore---/GlobalUICommont.h
+++ fanmore---/GlobalUICommont.h
@@ -10,10 +10,10 @@
 #define fanmore____GlobalUICommont_h
 
 /**服务器地址*/
-#define  MainURL @"http://apitest.51flashmall.com:8080/fanmoreweb/app"
-//@"http://newtask.fanmore.cn/app"
+#define  MainURL @"http://newtask.fanmore.cn/app"
 //@"http://newtask.fanmore.cn/app"
 //
+//@"http://apitest.51flashmall.com:8080/fanmoreweb/app"
 //@"http://192.168.0.23:8080/fanmoreweb/app"
 
 /**微信支付*/
