b2f760a8ab1760ac75445b7bd45d1dcf99795876
sxluohaibo <lhb@lhbdeMac-mini.local>
2015-07-10 15:10:15 +0800

    3213123

--- fanmore---/GlobalUICommont.h
+++ fanmore---/GlobalUICommont.h
@@ -10,12 +10,10 @@
 #define fanmore____GlobalUICommont_h
 
 /**服务器地址*/
-<<<<<<< HEAD
-#define  MainURL 
+
+#define  MainURL @"http://newtask.fanmore.cn/app"
 //@"http://apitest.51flashmall.com:8080/fanmoreweb/app"
-=======
-#define  MainURL @"http://apitest.51flashmall.com:8080/fanmoreweb/app"
->>>>>>> develop
+
 //@"http://newtask.fanmore.cn/app"
 //@"http://apitest.51flashmall.com:8080/fanmoreweb/app"
 //@"http://newtask.fanmore.cn/app"
