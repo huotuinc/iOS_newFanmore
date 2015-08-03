b5311eb6e1a9d66dad197bcfae5942b727a61aca
sxluohaibo <lhb@lhbdeMac-mini.local>
2015-08-03 16:44:01 +0800

    修改本地数据

--- fanmore---/AppDelegate.m
+++ fanmore---/AppDelegate.m
@@ -521,8 +521,8 @@ static NSString *message = @"有一条新消息";
             NSString * path = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject];
             NSString *fileName = [path stringByAppendingPathComponent:LocalUserDate];
             userData* user =  [NSKeyedUnarchiver unarchiveObjectWithFile:fileName];
-            user.balance = [NSString stringWithFormat:@"%.1f", [user.balance doubleValue] - [type doubleValue]];
-            
+            user.balance = [NSString stringWithFormat:@"%.1f", [user.balance doubleValue] + [type doubleValue]];
+            [NSKeyedArchiver archiveRootObject:user toFile:fileName];
             self.getSendMes = YES;
             
             break;
@@ -584,6 +584,7 @@ static NSString *message = @"有一条新消息";
                 NSString *fileName = [path stringByAppendingPathComponent:LocalUserDate];
                 userData* user =  [NSKeyedUnarchiver unarchiveObjectWithFile:fileName];
                 user.balance = [NSString stringWithFormat:@"%.1f", [user.balance doubleValue] - [type doubleValue]];
+                [NSKeyedArchiver archiveRootObject:user toFile:fileName];
                 
                 if ([self.currentVC isKindOfClass:[MenuViewController class]]) {
                     MenuViewController *menu  = (MenuViewController *)self.currentVC;
